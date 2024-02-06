1850c434bd45a288d20c5ceb5bf27fa8f3673a94
paramanandnarsinhabhat <141020967+paramanandnarsinhabhat@users.noreply.github.com>
2024-02-06 14:19:07 +0530

    commit

---
+++ source/autotaggingsystemgru.py
@@ -0,0 +1,14 @@
+#string matching
+import re 
+
+#reading files
+import pandas as pd
+
+#handling html data
+from bs4 import BeautifulSoup
+
+#visualization
+import matplotlib.pyplot as plt  
+
+pd.set_option('display.max_colwidth', 200)
+
